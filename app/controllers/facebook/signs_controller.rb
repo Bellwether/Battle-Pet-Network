309d3ec08cd9d92788b89c6b88c76ce2d86730ca
Travis Dunn <travidunn@prospero.local>
2010-05-15 10:58:23 +0200

    sign control

--- app/controllers/facebook/signs_controller.rb
+++ app/controllers/facebook/signs_controller.rb
@@ -2,14 +2,15 @@ class Facebook::SignsController < Facebook::FacebookController
   before_filter :ensure_application_is_installed_by_facebook_user, :ensure_has_pet
   
   def create
-    # @sign = current_user_pet.buil
-    # 
-    # if @biography.save
-    #   flash[:notice] = "What a story! #{current_user_pet.name}'s biography has been written."
-    #   facebook_redirect_to facebook_pets_path(current_user_pet)
-    # else
-    #   flash[:error] = "Couldn't save biography! :("
-    #   render :action => :new
-    # end
+    params[:sign] ||= {}
+    sign_type = (params[:sign_type] ||= params[:sign][:sign_type])
+    @sign = current_user_pet.signings.build(:recipient_id => params[:pet_id], :sign_type => sign_type)
+    
+    if @sign.save
+      flash[:notice] = "Sent #{@sign.sign_type}."
+    else
+      flash[:error] = "Couldn't send sign! :( #{@sign.errors.full_messages}"
+    end
+    redirect_facebook_back
   end
 end
--- test/functional/facebook/signs_controller_test.rb
+++ test/functional/facebook/signs_controller_test.rb
@@ -4,11 +4,26 @@ class Facebook::SignsControllerTest  < ActionController::TestCase
   include Facebooker::Rails::TestHelpers
   
   def setup
+    @sender = pets(:siamese)
+    @recipient = pets(:burmese)
+    @user = @sender.user
   end
   
   def test_create
+    mock_user_facebooking(@user.facebook_id)
+    assert_difference ['Sign.count','@sender.signings.count','@recipient.signs.count'], +1 do
+      facebook_post :create, :pet_id => @recipient.id, :sign_type => 'purr', :fb_sig_user => @user.facebook_id
+      assert_response :success
+      assert flash[:notice]
+    end
   end
   
   def test_fail_create
+    mock_user_facebooking(@user.facebook_id)
+    assert_no_difference ['Sign.count','@sender.signings.count','@recipient.signs.count'] do
+      facebook_post :create, :pet_id => @recipient.id, :fb_sig_user => @user.facebook_id
+      assert_response :success
+      assert flash[:error]
+    end
   end
 end
