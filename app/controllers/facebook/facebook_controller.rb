39255dce573ececbb9c35946c57a597bb7b03ffc
Travis Dunn <travidunn@prospero.local>
2010-06-03 18:53:32 +0200

    facebook fixes

--- app/controllers/facebook/facebook_controller.rb
+++ app/controllers/facebook/facebook_controller.rb
@@ -9,6 +9,10 @@ class Facebook::FacebookController < ApplicationController
   before_filter :ensure_facebook_request, :set_facebook_user
   after_filter :store_location
   
+  # # #
+  # pet or user (or anonymous) authentication questions for display
+  # # #    
+  
   def has_pet?
     current_user_pet && current_user_pet != nil
   end
@@ -25,23 +29,26 @@ class Facebook::FacebookController < ApplicationController
     return (facebook_session != nil && facebook_session.secured?)
   end
   
+  # # #
+  # overrides to handle facebook routing and the 'facebook' routes namespace  
+  # # #  
+  
   def facebook_redirect_to(url)
     redirect_to url.blank? ? '' : url.replace('facebook/','')
   end  
   
   def store_location
-    logger.info "method = #{request.method}"
-    logger.info "+++++++ request.request_uri = #{request.request_uri}"
-    session[:return_to] = request.request_uri.replace('facebook/', '') if request.method == 'get'
-    logger.info "+++++++ stored_location for #{stored_location} == #{session[:return_to]}"
+    logger.info "+++++++ store_location: request_uri will be #{request.request_uri.gsub('facebook/', '')} if #{(request.method.to_s == 'get')} (#{request.method.to_s})"
+    session[:return_to] = request.request_uri.gsub('facebook/', '') if request.method.to_s == 'get'
   end
 
   def stored_location
-    session[:return_to] ||= facebook_root_path.replace('facebook/','')
+    logger.info "+++++++ stored_location: session has #{session[:return_to]} (default: #{facebook_root_path.gsub('facebook/','')})"
+    session[:return_to] ||= facebook_root_path.gsub('facebook/','')
   end
 
   def redirect_facebook_back
-    logger.info "+++++++ stored_location = #{stored_location}"
+    logger.info "+++++++ redirect_facebook_back: stored location is #{stored_location}"
     redirect_to stored_location, :status => :ok
   end
   
@@ -68,5 +75,15 @@ class Facebook::FacebookController < ApplicationController
         @current_user ||= User.from_facebook(facebook_session.user.uid.to_i,facebook_session)
       end
     end
+  end
+  
+  # # #
+  # handling various invalid requests gracefully in faacebook
+  # # #
+    
+  rescue_from ActionController::MethodNotAllowed, :with => :rescue_from_missing_method
+
+  def rescue_from_missing_method(exception)
+    facebook_redirect_to facebook_root
   end
 end
