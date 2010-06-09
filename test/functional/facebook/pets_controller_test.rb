ead400546d3b160ce8e993e0a403132b9328be37
Travis Dunn <travidunn@prospero.local>
2010-06-09 18:24:44 +0200

    facebook fixes

--- test/functional/facebook/pets_controller_test.rb
+++ test/functional/facebook/pets_controller_test.rb
@@ -120,6 +120,10 @@ class Facebook::PetsControllerTest  < ActionController::TestCase
     assert assigns(:gear)
     assert_tag :tag => "table", :attributes => { :id => "combat-profile" }
     assert_tag :tag => "table", :attributes => { :id => "advancements" }
+    assert_tag :tag => "table", :attributes => { :class => "challenge" }, :descendant => {
+      :tag => "a", :attributes => { :href => "/#{@controller.facebook_app_path}/pets/home/challenges/#{assigns(:challenges).first.id}/refuse"},
+      :tag => "a", :attributes => { :href => "/#{@controller.facebook_app_path}/pets/home/challenges/#{assigns(:challenges).first.id}/edit"}
+    }
     assert_tag :tag => "div", :attributes => { :class => "box gear" }
     assert_tag :tag => "form", :descendant => {
       :tag => "input", :attributes => { :name => "_method", :type => "hidden", :value => "put" }
