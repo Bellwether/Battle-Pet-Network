99b29170942c7c5413ee01c410bfc869792859ee
Travis Dunn <travidunn@prospero.local>
2010-06-26 13:11:13 +0200

    awards

--- app/models/achivement/award.rb
+++ app/models/achivement/award.rb
@@ -1,3 +1,39 @@
 class Award < ActiveRecord::Base
   belongs_to :leaderboard
+  
+  AWARD_TYPES = %w(kibble item)
+  
+  attr_accessor :rankable  
+  
+  validates_inclusion_of :award_type, :in => AWARD_TYPES
+    
+  def award_rankable(rankable)
+    @rankable = rankable
+    if award_type == 'kibble'
+      award_kibble
+    elsif award_type == 'item'
+      award_item
+    end
+  end
+  
+  def description
+    case award_type
+    when 'kibble'
+      "+#{prize} kibble"
+    end
+  end
+  
+protected  
+  
+  def award_kibble
+    if @rankable.respond_to?(:kibble)
+      @rankable.update_attribute(:kibble, [@rankable.kibble + prize.to_i, 0].max)
+    elsif @rankable.respond_to?(:pet)
+      @rankable.pet.update_attribute(:kibble, [@rankable.pet.kibble + prize.to_i, 0].max)
+    end
+  end
+  
+  def award_item
+    true
+  end
 end
---
+++ test/unit/award_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class AwardTest < ActiveSupport::TestCase
+  def setup
+    @pet = pets(:siamese)
+    @pack = packs(:alpha)
+    @shop = shops(:first)
+    @rankables = [@pet,@pack,@shop]
+  end
+  
+  def test_description
+    Award::AWARD_TYPES.each do |t|
+      award = Award.find_by_award_type(t)
+      next unless award
+      assert !award.description.blank?
+    end
+  end
+  
+  def test_award_rankable
+    Award::AWARD_TYPES.each do |t|
+      award = Award.find_by_award_type(t)
+      next unless award
+      @rankables.each do |r|
+        if award.award_type == 'kibble'
+          assert_difference [(r.respond_to?(:kibble) ? 'r.kibble' : 'r.pet.kibble')], +award.prize.to_i do
+            award.award_rankable(r)
+          end
+        end
+      end
+    end
+  end
+end
