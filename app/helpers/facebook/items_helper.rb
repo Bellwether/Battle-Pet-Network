67cc5f1fd21b0f64e171bb1222caedeb386c10ac
Travis Dunn <travidunn@prospero.local>
2010-06-29 21:17:09 +0200

    facebook fixes

--- app/helpers/facebook/items_helper.rb
+++ app/helpers/facebook/items_helper.rb
@@ -1,12 +1,11 @@
 module Facebook::ItemsHelper
-  def item_store_table(item_type, description)
+  def item_store_table(item_type, alt)
     avatar_path = "items/types/medium/#{item_type.downcase}.png"
     return "<table class='item-store'>" <<
-      "<tr><td rowspan='3'>#{facebook_image_tag(avatar_path)}</td>" <<
+      "<tr><td rowspan='3'>#{facebook_image_tag(avatar_path, :alt => alt)}</td>" <<
       "<td><strong>#{item_type} Store</strong></td></tr>" <<
       "<tr><td>#{Item.type_is(item_type).in_stock.count} types, #{Item.type_is(item_type).in_stock.sum(:stock)} in stock</td></tr>" <<
       "<tr><td><span class='shopping-button'>#{facebook_link_to('Go Shopping',store_facebook_item_path(item_type))}</span></td></tr>" <<
-      "<tr><td colspan='2'>#{description}</td></tr>" <<
       "</table>"
   end
 end
