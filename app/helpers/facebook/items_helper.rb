f60f8a7377052434419ee5b301b34f729f36b978
Travis Dunn <travidunn@prospero.local>
2010-07-17 07:38:55 +0200

    css buttons

--- app/helpers/facebook/items_helper.rb
+++ app/helpers/facebook/items_helper.rb
@@ -5,7 +5,7 @@ module Facebook::ItemsHelper
       "<tr><td rowspan='3'>#{facebook_image_tag(avatar_path, :title => alt)}</td>" <<
       "<td><strong>#{item_type} Store</strong></td></tr>" <<
       "<tr><td>#{pluralize(Item.type_is(item_type).in_stock.count, 'types')}<br />#{Item.type_is(item_type).in_stock.sum(:stock)} in stock</td></tr>" <<
-      "<tr><td><span class='shopping-button'>#{facebook_link_to('Go Shopping',store_facebook_item_path(item_type))}</span></td></tr>" <<
+      "<tr><td>#{facebook_link_to('Go Shopping',store_facebook_item_path(item_type), :class => 'button gray small')}</td></tr>" <<
       "</table>"
   end
 end
