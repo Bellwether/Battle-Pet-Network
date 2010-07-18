52f8a25e12ec094e1c00561bf0feca71a29e8848
Travis Dunn <travidunn@prospero.local>
2010-07-18 03:02:33 +0200

    facebook css

--- app/helpers/facebook/items_helper.rb
+++ app/helpers/facebook/items_helper.rb
@@ -8,4 +8,25 @@ module Facebook::ItemsHelper
       "<tr><td>#{facebook_link_to('Go Shopping',store_facebook_item_path(item_type), :class => 'button gray small')}</td></tr>" <<
       "</table>"
   end
+  
+  def item_badge(item, &proc)
+    html = "<table class='item'>" <<
+           "<thead>" <<
+              "<tr><th>#{item.name}</th></tr>" <<
+            "</thead>" <<
+              "<tbody>" <<
+              "<tr>" <<
+                "<td colspan='4'>" <<
+                avatar_image(item,'small') <<
+                "</td>" <<
+                "<td><label>Type: <span>#{item.item_type}</span></td>" <<
+              "</tr>" <<
+              "<tr><td><label>Power: <span>#{item.power}</span></td></tr>" <<
+              "<tr><td><label>Rarity: <span>#{item.cost}</span></td></tr>" <<
+              "<tr><td><label>Cost: <span>#{item.cost}</span></td></tr>"
+            
+    concat("#{html}<tfoot>")
+    proc.call(item) if proc
+    concat("</tfoot></table>")
+  end
 end
